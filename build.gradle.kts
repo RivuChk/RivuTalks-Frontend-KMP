08d94abac24a072ac62e678bfafd156999f53bd7
Rivu Chakraborty <rivu.chakraborty6174@gmail.com>
2023-01-24 10:08:21 +0530

    fix build: update IDEA version and AGP: https://github.com/joreilly/PeopleInSpace/issues/187

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 
     dependencies {
         // keeping this here to allow AS to automatically update
-        classpath("com.android.tools.build:gradle:7.4.0")
+        classpath("com.android.tools.build:gradle:7.4.0-beta02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
 
